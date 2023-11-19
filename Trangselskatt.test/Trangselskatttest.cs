99c168a3f5c9442385a92fedd29d2234784eead3
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2023-11-19 21:13:19 +0100

    Glömt att pusha men här kommer alla steg

--- Trangselskatt.test/Trangselskatttest.cs
+++ Trangselskatt.test/Trangselskatttest.cs
@@ -1,12 +1,137 @@
-using Trangselskatt;
-using System;
 using NUnit.Framework;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Trangselskatt;
 
 namespace Trangselskatt.test
 {
     [TestFixture]
-    public class Trangselskatttest
+    public class TrangselskattTest
     {
-        
+        [TestCase("2023-05-31 06:15", "Total avgiften är 8 kr")]
+        [TestCase("2023-05-31 07:45", "Total avgiften är 18 kr")]
+        [TestCase("2023-05-31 15:15", "Total avgiften är 13 kr")]
+        [TestCase("2023-05-31 18:45", "Total avgiften är 0 kr")]
+        public void TestaRäknaTotalBeloppEnskildTid(string datumTid, string forvantatResultat)
+        {
+            // Arrange
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Act
+            Metoder.RäknaTotalBelopp(datumTid);
+            var resultat = sw.ToString().Trim();
+
+            // Assert
+            Assert.That(resultat, Is.EqualTo(forvantatResultat));
+        }
+
+
+        [Test]
+        public void TestaRäknaTotalBeloppFleraTider()
+        {
+            // Arrange
+            var input = "2023-05-31 06:15, 2023-05-31 07:45, 2023-05-31 15:15, 2023-05-31 18:45";
+            var forvantatResultat = "Total avgiften är 39 kr";
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Act
+            Metoder.RäknaTotalBelopp(input);
+            var resultat = sw.ToString().Trim();
+
+            // Assert
+            Assert.That(resultat, Is.EqualTo(forvantatResultat));
+        }
+
+
+        [Test]
+        public void TestaParseInputData()
+        {
+            //Arrange
+            var input = "2023-05-31 06:15, felaktigt datum, 2023-05-31 07:45";
+            var forvantadeTider = new List<DateTime>
+            {
+                new DateTime(2023, 5, 31, 6, 15, 0),
+                new DateTime(2023, 5, 31, 7, 45, 0)
+            };
+
+            // Act
+            var resultat = Metoder.ParseInputData(input);
+
+            // Assert
+            Assert.That(resultat, Is.EqualTo(forvantadeTider));
+        }
+
+
+        [Test]
+        public void TestaBeräknaDagligaAvgifter()
+        {
+            //Arrange
+            var tidpunkter = new List<DateTime>
+            {
+                new DateTime(2023, 5, 31, 6, 15, 0),
+                new DateTime(2023, 5, 31, 7, 45, 0),
+                new DateTime(2023, 5, 31, 15, 15, 0)
+            };
+            var forvantatResultat = new Dictionary<DateTime, int>
+            {
+                { new DateTime(2023, 5, 31), 39 }
+            };
+
+            // Act
+            var resultat = Metoder.BeräknaDagligaAvgifter(tidpunkter);
+
+            // Assert
+            Assert.That(resultat, Is.EqualTo(forvantatResultat));
+
+        }
+
+        [Test]
+        public void TestaRäknaTotalBeloppPåAvgiftsfriaDagar()
+        {
+            var testData = new List<(string Datum, string FörväntatResultat)>
+            {
+             ("2023-06-03", "Total avgiften är 0 kr"), // Lördag
+             ("2023-06-04", "Total avgiften är 0 kr"), // Söndag
+             ("2023-07-01", "Total avgiften är 0 kr")  // Juli
+            };
+
+            foreach (var (datum, förväntatResultat) in testData)
+            {
+                // Arrange
+                var sw = new StringWriter();
+                Console.SetOut(sw);
+
+                // Act
+                Metoder.RäknaTotalBelopp(datum);
+                var resultat = sw.ToString().Trim();
+
+                // Assert
+                Assert.That(resultat, Is.EqualTo(förväntatResultat), $"Test misslyckades för datum: {datum}");
+
+            }
+        }
+
+        [Test]
+        public void TestaRäknaTotalBeloppMedFleraPassagerInom60Minuter()
+        {
+            //Arrange
+            var input = "2023-05-31 06:20, 2023-05-31 06:45, 2023-05-31 07:10"; // Avgifter: 8 kr, 13 kr, 18 kr
+            var forvantatResultat = "Total avgiften är 18 kr"; // Högsta avgift inom första 60 minuter
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            //Act
+            Metoder.RäknaTotalBelopp(input);
+            var resultat = sw.ToString().Trim();
+
+            //Assert
+            Assert.That(resultat, Is.EqualTo(forvantatResultat));
+
+        }
+
+
     }
 }
--- Trangselskatt/Metoder.cs
+++ Trangselskatt/Metoder.cs
@@ -1,9 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Trangselskatt
 {
     public class Metoder
     {
+        private static readonly Dictionary<(TimeSpan Start, TimeSpan Slut), int> Avgiftsintervall = new Dictionary<(TimeSpan, TimeSpan), int>
+        {
+            { (new TimeSpan(6, 0, 0), new TimeSpan(6, 29, 59)), 8 },
+            { (new TimeSpan(6, 30, 0), new TimeSpan(6, 59, 59)), 13 },
+            { (new TimeSpan(7, 0, 0), new TimeSpan(7, 59, 59)), 18 },
+            { (new TimeSpan(8, 0, 0), new TimeSpan(8, 29, 59)), 13 },
+            { (new TimeSpan(8, 30, 0), new TimeSpan(14, 59, 59)), 8 },
+            { (new TimeSpan(15, 0, 0), new TimeSpan(15, 29, 59)), 13 },
+            { (new TimeSpan(15, 30, 0), new TimeSpan(16, 59, 59)), 18 },
+            { (new TimeSpan(17, 0, 0), new TimeSpan(17, 59, 59)), 13 },
+            { (new TimeSpan(18, 0, 0), new TimeSpan(18, 29, 59)), 8 },
+            { (new TimeSpan(18, 30, 0), new TimeSpan(5, 59, 59)), 0 }
+        };
 
+        public static void RäknaTotalBelopp(string datumTider)
+        {
+            var tidpunkter = ParseInputData(datumTider);
+            var dagligaAvgifter = BeräknaDagligaAvgifter(tidpunkter);
+
+            int totalAvgift = dagligaAvgifter.Values.Sum();
+            Console.WriteLine($"Total avgiften är {totalAvgift} kr");
+        }
+
+        public static List<DateTime> ParseInputData(string datumTider)
+        {
+            return datumTider.Split(',')
+            .Select(dt => dt.Trim())
+            .Select(dtStr =>
+            {
+            if (DateTime.TryParseExact(dtStr, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime tidpunkt))
+            {
+              return (DateTime?)tidpunkt;
+            }
+              return null;
+            })
+             .Where(dt => dt.HasValue)
+             .Select(dt => dt!.Value)
+             .ToList();
+        }
+
+
+        public static Dictionary<DateTime, int> BeräknaDagligaAvgifter(List<DateTime> tidpunkter)
+        {
+            var dagligaAvgifter = new Dictionary<DateTime, int>();
+            DateTime? senasteDebiteradeTidpunkt = null;
+            int senasteAvgift = 0;
+
+            foreach (var tidpunkt in tidpunkter.OrderBy(t => t))
+            {
+                if (senasteDebiteradeTidpunkt.HasValue && tidpunkt < senasteDebiteradeTidpunkt.Value.AddMinutes(60))
+                {
+                    // Inom 60 minuter från senaste passage, uppdatera endast om högre avgift
+                    int aktuellAvgift = BeräknaAvgift(tidpunkt);
+                    if (aktuellAvgift > senasteAvgift)
+                    {
+                        dagligaAvgifter[tidpunkt.Date] = dagligaAvgifter[tidpunkt.Date] - senasteAvgift + aktuellAvgift;
+                        senasteAvgift = aktuellAvgift;
+                    }
+                }
+                else
+                {
+                    // Ny debitering
+                    senasteDebiteradeTidpunkt = tidpunkt;
+                    senasteAvgift = BeräknaAvgift(tidpunkt);
+                    if (dagligaAvgifter.ContainsKey(tidpunkt.Date))
+                    {
+                        dagligaAvgifter[tidpunkt.Date] += senasteAvgift;
+                    }
+                    else
+                    {
+                        dagligaAvgifter[tidpunkt.Date] = senasteAvgift;
+                    }
+                }
+            }
+
+            return dagligaAvgifter;
+        }
+
+        public static int BeräknaAvgift(DateTime tidpunkt)
+        {
+            if (ÄrAvgiftsfriDag(tidpunkt))
+            {
+                return 0;
+            }
+
+            TimeSpan tid = tidpunkt.TimeOfDay;
+            foreach (var intervall in Avgiftsintervall)
+            {
+                if (tid >= intervall.Key.Start && tid < intervall.Key.Slut)
+                {
+                    return intervall.Value;
+                }
+            }
+
+            return 0;
+        }
+
+        public static bool ÄrAvgiftsfriDag(DateTime datum)
+        {
+            return datum.DayOfWeek == DayOfWeek.Saturday || datum.DayOfWeek == DayOfWeek.Sunday || datum.Month == 7;
+        }
     }
 }
